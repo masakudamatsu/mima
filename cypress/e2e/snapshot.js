78d2915d73f96b90184c2e1c8a61dbab42e2c424
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-08-15 15:43:35 +0900

    style(percy): remove redundant comments
    These comments are left over from the repository that provides the base code for this project

--- cypress/e2e/snapshot.js
+++ cypress/e2e/snapshot.js
@@ -1,8 +1,3 @@
-// let scrollToBottom = require('scroll-to-bottomjs'); // to deal with lazy-loading images. see https://docs.percy.io/docs/capturing-lazy-loading-images
-// scroll-to-bottomjs appears to fix the scroll position at the bottom
-// so cy.scrollTo('top') doesn't appear to work afterwards, to reveal the top app bar
-// we instead use cy.scrollTo('bottom', {duration: 2000})
-
 describe('Integration test with visual testing', () => {
   it('Loads the homepage', () => {
     cy.visit('/');
