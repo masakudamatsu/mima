04a4fbcc0871980360131b554a2ffaf73b2137db
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-08-15 11:35:42 +0900

    docs: revise documentation in package.json and metadata.js
    remove the documentation inherited from the repository from which this project's initial code is
    derived

--- src/utils/metadata.js
+++ src/utils/metadata.js
@@ -1,5 +1,5 @@
 export const index = {
   title: 'My Ideal Map App',
   description:
-    'Progressive web app that allows you to customize Google Maps with placemarks of your own interest that link to web pages of your choice',
+    'Progressive web app that allows you to customize Google Maps with placemarks of your own interest that can be linked to web pages of your choice',
 };
