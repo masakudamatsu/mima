78d85b58a292a93806b198106f54581e24760c20
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-08-16 14:28:24 +0900

    refactor: remove dotenv and use Next.js built-in env.local for API key
    dotenv doesn't work with headless Chromium for Percy and Cloudflare Pages for deployment

--- cypress/e2e/snapshot.js
+++ cypress/e2e/snapshot.js
@@ -1,11 +1,7 @@
-const apikey = Cypress.env('apikey');
-const googleMapApiUrl = `https://maps.googleapis.com/maps/api/js?callback=__googleMapsCallback&key=${apikey}&v=weekly`;
-
 describe('Integration test with visual testing', () => {
   it('Loads the homepage', () => {
-    cy.intercept(googleMapApiUrl).as('googleMapApi');
     cy.visit('/');
-    cy.wait('@googleMapApi'); // Wait for Google Maps to be loaded;
+    cy.contains('Map Data'); // Bottom-right text to be rendered in Google Maps
     cy.percySnapshot('index', {widths: [320, 768, 1024]});
   });
 });
--- next.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-const {parsed: myEnv} = require('dotenv').config();
-
-module.exports = {
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-    return config;
-  },
-};
