4f080dd162f1b0c4292dcdcc8fc94e01568ebaa8
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-08-28 17:40:36 +0900

    test: write visual regression tests for switching map style by time of day
    There is no other way to test whether the map style switches between light and dark modes by the
    hours of the day

--- cypress/e2e/snapshot.js
+++ cypress/e2e/snapshot.js
@@ -1,7 +1,14 @@
-describe('Integration test with visual testing', () => {
-  it('Loads the homepage', () => {
+describe('Initial UI', () => {
+  it('Rendered in Light Mode at Daytime', () => {
+    cy.clock(Date.UTC(2021, 8, 28, 6), ['Date']); // https://docs.cypress.io/api/commands/clock#Function-names
     cy.visit('/');
     cy.contains('Map Data'); // Bottom-right text to be rendered in Google Maps
-    cy.percySnapshot('index', {widths: [320, 768, 1024]});
+    cy.percySnapshot('initial-ui-daytime', {widths: [320, 768, 1024]});
+  });
+  it('Rendered in Dark Mode at Nighttime', () => {
+    cy.clock(Date.UTC(2021, 8, 28, 18), ['Date']); // https://docs.cypress.io/api/commands/clock#Function-names
+    cy.visit('/');
+    cy.contains('Map Data'); // Bottom-right text to be rendered in Google Maps
+    cy.percySnapshot('initial-ui-nighttime', {widths: [320, 768, 1024]});
   });
 });
