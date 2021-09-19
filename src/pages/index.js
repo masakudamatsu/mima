bbd246c9fb004fe683b1e9a5797091e06481529d
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-09-19 15:51:47 +0900

    refactor(nightmode): switch nightmode on and off with React Context
    allow styled components, typically deep down the component tree, to access to the nightmode
    indicator, without prop-drillings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
-import {useState} from 'react';
 import {index} from 'src/utils/metadata';
+
+import {NightModeProvider} from 'src/context/NightModeContext';
 
 import LocatorButton from 'src/components/LocatorButton';
 import Map from 'src/components/Map';
@@ -10,14 +11,6 @@ import SavePlaceButton from 'src/components/SavePlaceButton';
 import SearchButton from 'src/components/SearchButton';
 
 function HomePage() {
-  let nightMode;
-  const currentTime = new Date();
-  const currentHour = currentTime.getHours();
-  if (currentHour < 6 || currentHour >= 18) {
-    nightMode = true;
-  } else {
-    nightMode = false;
-  }
   return (
     <>
       <Head>
@@ -25,11 +18,13 @@ function HomePage() {
         <meta name="description" content={index.description} />
       </Head>
       <Noscript />
-      <MenuButton />
-      <SearchButton />
-      <LocatorButton />
-      <SavePlaceButton />
-      <Map nightMode={nightMode} />
+      <NightModeProvider>
+        <MenuButton />
+        <SearchButton />
+        <LocatorButton />
+        <SavePlaceButton />
+        <Map />
+      </NightModeProvider>
     </>
   ); // see https://codepen.io/masakudamatsu/pen/QWpbELb
 }
