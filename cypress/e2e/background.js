2dc13cf52cba061c4389fd71fbe425b4d8834503
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-10-06 16:35:21 +0900

    test(map): test if <body> background-color is set as expected by the time of the day
    make sure the screen color during the map loading switches by day or night

---
+++ cypress/e2e/background.js
@@ -0,0 +1,14 @@
+import {map} from '../../src/utils/designtokens';
+
+describe('While loading the map', () => {
+  it('at daytime', () => {
+    cy.clock(Date.UTC(2021, 8, 28, 6), ['Date']); // https://docs.cypress.io/api/commands/clock#Function-names
+    cy.visit('/');
+    cy.get('body').should('have.css', 'background-color', map.cityblocks.day);
+  });
+  it('at nighttime', () => {
+    cy.clock(Date.UTC(2021, 8, 28, 18), ['Date']); // https://docs.cypress.io/api/commands/clock#Function-names
+    cy.visit('/');
+    cy.get('body').should('have.css', 'background-color', map.cityblocks.night);
+  });
+});
