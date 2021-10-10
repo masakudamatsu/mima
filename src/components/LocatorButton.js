fd1e4e0e3e8a95d3b2d213fc9ac44b88bfb9eeae
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-10-10 09:57:32 +0900

    perf(locatorbutton): use cached GPS info up to 1 second, to show user's current location
    speed up the feature implementation; people walk at 1.4m per second, which is roughly the same as
    the accuracy error range

--- src/components/LocatorButton.js
+++ src/components/LocatorButton.js
@@ -19,61 +19,65 @@ const LocatorButton = ({mapObject}) => {
     if (navigator.geolocation) {
       // Start blinking the button
       setLoading(true);
-      navigator.geolocation.getCurrentPosition(position => {
-        // obtain current location geocoordinate
-        const currentPosition = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          // obtain current location geocoordinate
+          const currentPosition = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
 
-        // Move the map to the current location
-        mapObject.setCenter(currentPosition);
+          // Move the map to the current location
+          mapObject.setCenter(currentPosition);
 
-        // prepare for drawing markers
-        const google = window.google;
+          // prepare for drawing markers
+          const google = window.google;
 
-        // Mark the current location
-        if (marker) {
-          marker.setMap(null); // remove the previous current location marker from the map
-        }
-        const blueCircle = {
-          // design the icon to mark the current location
-          // source: lines 50-55 of https://github.com/ChadKillingsworth/geolocation-marker/releases/download/v2.0.5/geolocation-marker.js
-          path: google.maps.SymbolPath.CIRCLE,
-          fillColor: color['google-blue 100'],
-          fillOpacity: 1,
-          scale: 8,
-          strokeColor: color['white 100'],
-          strokeWeight: 2,
-        };
-        marker = new google.maps.Marker({
-          icon: blueCircle,
-          position: currentPosition,
-          title: 'You are here!',
-        });
-        marker.setMap(mapObject);
+          // Mark the current location
+          if (marker) {
+            marker.setMap(null); // remove the previous current location marker from the map
+          }
+          const blueCircle = {
+            // design the icon to mark the current location
+            // source: lines 50-55 of https://github.com/ChadKillingsworth/geolocation-marker/releases/download/v2.0.5/geolocation-marker.js
+            path: google.maps.SymbolPath.CIRCLE,
+            fillColor: color['google-blue 100'],
+            fillOpacity: 1,
+            scale: 8,
+            strokeColor: color['white 100'],
+            strokeWeight: 2,
+          };
+          marker = new google.maps.Marker({
+            icon: blueCircle,
+            position: currentPosition,
+            title: 'You are here!',
+          });
+          marker.setMap(mapObject);
 
-        // Draw the circle indicationg the 95% confidence interval of current position
-        const accuracyRange = position.coords.accuracy; // obtain the 95% confidence interval
-        if (accuracyCircle) {
-          accuracyCircle.setMap(null); // remove the previous circle from the map
-        }
-        accuracyCircle = new google.maps.Circle({
-          // source: lines 65-72 of https://github.com/ChadKillingsworth/geolocation-marker/releases/download/v2.0.5/geolocation-marker.js
-          center: currentPosition,
-          fillColor: color['google-blue-dark 100'],
-          fillOpacity: 0.4,
-          radius: accuracyRange,
-          strokeColor: color['google-blue-light 100'],
-          strokeOpacity: 0.4,
-          strokeWeight: 1,
-          zIndex: 1,
-        });
-        accuracyCircle.setMap(mapObject);
+          // Draw the circle indicationg the 95% confidence interval of current position
+          const accuracyRange = position.coords.accuracy; // obtain the 95% confidence interval
+          if (accuracyCircle) {
+            accuracyCircle.setMap(null); // remove the previous circle from the map
+          }
+          accuracyCircle = new google.maps.Circle({
+            // source: lines 65-72 of https://github.com/ChadKillingsworth/geolocation-marker/releases/download/v2.0.5/geolocation-marker.js
+            center: currentPosition,
+            fillColor: color['google-blue-dark 100'],
+            fillOpacity: 0.4,
+            radius: accuracyRange,
+            strokeColor: color['google-blue-light 100'],
+            strokeOpacity: 0.4,
+            strokeWeight: 1,
+            zIndex: 1,
+          });
+          accuracyCircle.setMap(mapObject);
 
-        // Stop blinking the button
-        setLoading(false);
-      });
+          // Stop blinking the button
+          setLoading(false);
+        },
+        () => {},
+        {maximumAge: 1000}, // people walk at 1.4m per second; typically accurracy error is about the same (source: https://en.wikipedia.org/wiki/Preferred_walking_speed#:~:text=Many%20people%20tend%20to%20walk,they%20typically%20choose%20not%20to.)
+      );
     } else {
       // Browser doesn't support Geolocation
     }
