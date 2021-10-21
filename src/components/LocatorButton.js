fa3577d2476f33c4bb89132acb87defff888ba8f
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-10-21 21:50:16 +0900

    refactor(locatorbutton): stop hoisting helper function (with code cleaning)
    improve code readability

--- src/components/LocatorButton.js
+++ src/components/LocatorButton.js
@@ -1,10 +1,13 @@
 import {useContext, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 
+import ModalPopup from 'src/components/ModalPopup';
+
 import {NightModeContext} from 'src/context/NightModeContext';
 
 import {Button} from 'src/elements/Button';
 import SvgCloud from 'src/elements/SvgCloud';
+
 import {color} from 'src/utils/designtokens';
 import {
   geolocationNotSupported,
@@ -13,7 +16,6 @@ import {
   locatorButtonLabel,
   userLocationMakerLabel,
 } from 'src/utils/uiCopies';
-import ModalPopup from 'src/components/ModalPopup';
 
 let marker = null;
 let accuracyCircle = null;
@@ -21,6 +23,7 @@ let accuracyCircle = null;
 // because setStatus() causes the rerendering of the component, assigning null to these variables
 
 const flightIcon = {
+  // source: Material Icons Flight: https://fonts.google.com/icons?icon.query=flight
   height: 24,
   path: `
     M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z
@@ -32,69 +35,149 @@ const LocatorButton = ({mapObject}) => {
   // UI states
   const nightMode = useContext(NightModeContext);
   const [status, setStatus] = useState('initial');
+  // to track user location
+  const userLocation = useRef(null);
+  const userDirection = useRef(45); // to match the button label icon
 
-  const currentPosition = useRef(null);
-  const currentDirection = useRef(45); // to match the button label icon
+  // helper functions
+  function obtainCurrentCoordinates(position) {
+    return {lat: position.coords.latitude, lng: position.coords.longitude};
+  }
+  function obtainCurrentDirection(previousCoordinates, currentCoordinates) {
+    const diffLat = currentCoordinates.lat - previousCoordinates.lat;
+    const diffLng = currentCoordinates.lng - previousCoordinates.lng;
+    const anticlockwiseAngleFromEast = convertToDegrees(
+      Math.atan2(diffLat, diffLng), // Docs on Math.atan2(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/atan2
+    );
+    const clockwiseAngleFromNorth = 90 - anticlockwiseAngleFromEast; // East 0 => 90; North 90 => 0; West 180 => -90; South 270 => -180;
+    return clockwiseAngleFromNorth;
+    // helper function
+    function convertToDegrees(radian) {
+      return (radian * 180) / Math.PI; // https://www.101computing.net/radians-to-degrees-conversions/
+    }
+  }
+  function removeUserLocation() {
+    if (marker) {
+      marker.setMap(null); // remove the previous current location marker from the map
+    }
+    if (accuracyCircle) {
+      accuracyCircle.setMap(null); // remove the previous circle from the map
+    }
+  }
+  function markUserLocation({
+    userDirection,
+    userLocation,
+    mapObject,
+    markerLabelText,
+    svgIcon,
+    accuracy,
+  }) {
+    removeUserLocation(); // otherwise, previous location ramains visible
 
-  const trackCurrentLocation = () => {
+    // prepare for drawing markers
+    const google = window.google;
+
+    // design the icon to mark the current location
+    marker = new google.maps.Marker({
+      icon: {
+        anchor: new google.maps.Point(svgIcon.width / 2, svgIcon.height / 2),
+        fillColor: color['google-blue 100'],
+        fillOpacity: 1,
+        path: svgIcon.path,
+        rotation: userDirection.current,
+        scale: 2,
+        strokeColor: color['white 100'],
+        strokeWeight: 2,
+      },
+      position: userLocation.current,
+      title: markerLabelText,
+    });
+    // Mark the current location
+    marker.setMap(mapObject);
+
+    // Draw the circle indicationg the 95% confidence interval of current position
+    accuracyCircle = new google.maps.Circle({
+      // source: lines 65-72 of https://github.com/ChadKillingsworth/geolocation-marker/releases/download/v2.0.5/geolocation-marker.js
+      center: userLocation.current,
+      fillColor: color['google-blue-dark 100'],
+      fillOpacity: 0.4,
+      radius: accuracy,
+      strokeColor: color['google-blue-light 100'],
+      strokeOpacity: 0.4,
+      strokeWeight: 1,
+      zIndex: 1,
+    });
+    accuracyCircle.setMap(mapObject);
+  }
+  function handleGeolocationError(error, setStatus) {
+    if (error.code === 1) {
+      setStatus('permissionDenied');
+    } else if (error.code === 2) {
+      setStatus('positionUnavailable');
+    }
+  }
+
+  // event hanlders
+  const trackUserLocation = () => {
     if (navigator.geolocation) {
       // Start blinking the button
       setStatus('loading');
       // First-run of extracting GPS info
       navigator.geolocation.getCurrentPosition(
         position => {
-          currentPosition.current = obtainCurrentCoordinates(position);
-          markCurrentLocation({
-            currentDirection,
-            currentPosition,
+          userLocation.current = obtainCurrentCoordinates(position);
+          markUserLocation({
+            userDirection,
+            userLocation,
             mapObject,
             markerLabelText: userLocationMakerLabel,
-            position,
+            svgIcon: flightIcon,
+            accuracy: position.coords.accuracy,
           });
           // Move to the current location
-          mapObject.setCenter(currentPosition.current);
+          mapObject.setCenter(userLocation.current);
           // Stop blinking the button & Change the button label icon
           setStatus('watching');
           // Enable continuous extraction of GPS info
           navigator.geolocation.watchPosition(
             position => {
-              const previousCoordinates = currentPosition.current;
-              currentPosition.current = obtainCurrentCoordinates(position);
-              currentDirection.current = obtainCurrentDirection(
+              const previousCoordinates = userLocation.current;
+              userLocation.current = obtainCurrentCoordinates(position);
+              userDirection.current = obtainCurrentDirection(
                 previousCoordinates,
-                currentPosition.current,
+                userLocation.current,
               );
-              markCurrentLocation({
-                currentDirection,
-                currentPosition,
+              markUserLocation({
+                userDirection,
+                userLocation,
                 mapObject,
                 markerLabelText: userLocationMakerLabel,
-                position,
+                svgIcon: flightIcon,
+                accuracy: position.coords.accuracy,
               });
             },
             error => {
-              handleGeolocationError(error, setStatus, setModalPopupHidden);
+              handleGeolocationError(error, setStatus);
             },
             {maximumAge: 0},
           );
         },
         error => {
-          handleGeolocationError(error, setStatus, setModalPopupHidden);
+          handleGeolocationError(error, setStatus);
         },
         {maximumAge: 1000},
       );
     } else {
-      console.log('run');
       // Browser doesn't support Geolocation
       setStatus('geolocationNotSupported');
     }
   };
 
   const moveToCurrentLocation = () => {
-    mapObject.setCenter(currentPosition.current);
+    mapObject.setCenter(userLocation.current);
   };
 
-  const setModalPopupHidden = () => {
+  const initializeUI = () => {
     setStatus('initial');
   };
 
@@ -105,7 +188,7 @@ const LocatorButton = ({mapObject}) => {
           data-darkmode={nightMode}
           data-position="bottom-right-second"
           data-loading={status === 'loading'}
-          onClick={trackCurrentLocation}
+          onClick={trackUserLocation}
           type="button"
         >
           <SvgCloud icon={'flightTakeoff'} title={locatorButtonLabel.default} />
@@ -114,7 +197,6 @@ const LocatorButton = ({mapObject}) => {
         <Button
           data-darkmode={nightMode}
           data-position="bottom-right-second"
-          data-loading={status === 'loading'}
           onClick={moveToCurrentLocation}
           type="button"
         >
@@ -125,34 +207,34 @@ const LocatorButton = ({mapObject}) => {
         </Button>
       )}
       {status === 'permissionDenied' && (
-        <ModalPopup setModalPopupHidden={setModalPopupHidden}>
+        <ModalPopup setModalPopupHidden={initializeUI}>
           <h1>{geolocationPermissionDenied.what}</h1>
           <p>{geolocationPermissionDenied.why}</p>
           <p>{geolocationPermissionDenied.how}</p>
-          <button onClick={setModalPopupHidden} type="button">
+          <button onClick={initializeUI} type="button">
             {geolocationPermissionDenied.button}
           </button>
         </ModalPopup>
       )}
       {status === 'positionUnavailable' && (
-        <ModalPopup setModalPopupHidden={setModalPopupHidden}>
+        <ModalPopup setModalPopupHidden={initializeUI}>
           <h1>{geolocationPositionUnavailable.what}</h1>
           <p>{geolocationPositionUnavailable.why}</p>
           <p>{geolocationPositionUnavailable.how}</p>
-          <button onClick={trackCurrentLocation} type="button">
+          <button onClick={trackUserLocation} type="button">
             {geolocationPositionUnavailable.button.primary}
           </button>
-          <button onClick={setModalPopupHidden} type="button">
+          <button onClick={initializeUI} type="button">
             {geolocationPositionUnavailable.button.secondary}
           </button>{' '}
         </ModalPopup>
       )}
       {status === 'geolocationNotSupported' && (
-        <ModalPopup setModalPopupHidden={setModalPopupHidden}>
+        <ModalPopup setModalPopupHidden={initializeUI}>
           <h1>{geolocationNotSupported.what}</h1>
           <p>{geolocationNotSupported.why}</p>
           <p>{geolocationNotSupported.how}</p>
-          <button onClick={setModalPopupHidden} type="button">
+          <button onClick={initializeUI} type="button">
             {geolocationNotSupported.button}
           </button>
         </ModalPopup>
@@ -166,82 +248,3 @@ LocatorButton.propTypes = {
 };
 
 export default LocatorButton;
-
-// helper functions
-
-function obtainCurrentCoordinates(position) {
-  return {lat: position.coords.latitude, lng: position.coords.longitude};
-}
-
-function obtainCurrentDirection(previousCoordinates, currentCoordinates) {
-  const diffLat = currentCoordinates.lat - previousCoordinates.lat;
-  const diffLng = currentCoordinates.lng - previousCoordinates.lng;
-  const anticlockwiseAngleFromEast = convertToDegrees(
-    Math.atan2(diffLat, diffLng), // Docs on Math.atan2(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/atan2
-  );
-  const clockwiseAngleFromNorth = 90 - anticlockwiseAngleFromEast; // East 0 => 90; North 90 => 0; West 180 => -90; South 270 => -180;
-  return clockwiseAngleFromNorth;
-}
-function convertToDegrees(radian) {
-  return (radian * 180) / Math.PI; // https://www.101computing.net/radians-to-degrees-conversions/
-}
-
-function markCurrentLocation({
-  currentDirection,
-  currentPosition,
-  mapObject,
-  markerLabelText,
-  position,
-}) {
-  // prepare for drawing markers
-  const google = window.google;
-
-  // Mark the current location
-  if (marker) {
-    marker.setMap(null); // remove the previous current location marker from the map
-  }
-  const svgIcon = {
-    // design the icon to mark the current location
-    // source: Material Icons Flight: https://fonts.google.com/icons?icon.query=flight
-    anchor: new google.maps.Point(flightIcon.width / 2, flightIcon.height / 2),
-    fillColor: color['google-blue 100'],
-    fillOpacity: 1,
-    path: flightIcon.path,
-    rotation: currentDirection.current,
-    scale: 2,
-    strokeColor: color['white 100'],
-    strokeWeight: 2,
-  };
-  marker = new google.maps.Marker({
-    icon: svgIcon,
-    position: currentPosition.current,
-    title: markerLabelText,
-  });
-  marker.setMap(mapObject);
-
-  // Draw the circle indicationg the 95% confidence interval of current position
-  const accuracyRange = position.coords.accuracy; // obtain the 95% confidence interval
-  if (accuracyCircle) {
-    accuracyCircle.setMap(null); // remove the previous circle from the map
-  }
-  accuracyCircle = new google.maps.Circle({
-    // source: lines 65-72 of https://github.com/ChadKillingsworth/geolocation-marker/releases/download/v2.0.5/geolocation-marker.js
-    center: currentPosition.current,
-    fillColor: color['google-blue-dark 100'],
-    fillOpacity: 0.4,
-    radius: accuracyRange,
-    strokeColor: color['google-blue-light 100'],
-    strokeOpacity: 0.4,
-    strokeWeight: 1,
-    zIndex: 1,
-  });
-  accuracyCircle.setMap(mapObject);
-}
-
-function handleGeolocationError(error, setStatus) {
-  if (error.code === 1) {
-    setStatus('permissionDenied');
-  } else if (error.code === 2) {
-    setStatus('positionUnavailable');
-  }
-}
