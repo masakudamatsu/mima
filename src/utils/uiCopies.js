67ef9e6849bdb613c9cbb36565ec115277dcfac3
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-10-27 15:39:23 +0900

    fix(locatorbutton): revise Geolocation API error dialog messages
    use different phrases for each error case
    
    re #76

--- src/utils/uiCopies.js
+++ src/utils/uiCopies.js
@@ -13,22 +13,21 @@ export const buttonLabel = {
 
 export const menuLabel = 'Menu';
 
-const geolocationApiFails = 'Unable to find where you are';
 export const geolocationNotSupported = {
-  what: geolocationApiFails,
-  why: `We couldn’t obtain your location data from your browser.`,
+  what: `Unable to get location data from your browser`,
+  why: `Your browser cannot handle location data in your device.`,
   how: `Consider using other browsers such as Chrome, Firefox, Microsoft Edge, and Safari.`,
   button: `Got it`,
 };
 export const geolocationPermissionDenied = {
-  what: `We're not allowed to find where you are`,
+  what: `Not allowed to find where you are`,
   why: `You’ve chosen to keep your location private, and we respect your privacy.`,
   how: `To see where you are on the map, enable location services with your OS/browser.`,
   button: `Got it`,
 };
 export const geolocationPositionUnavailable = {
-  what: geolocationApiFails,
-  why: `We couldn’t obtain your location data from your device.`,
+  what: `Unable to get location data from your device`,
+  why: `Your device has an issue with location data.`,
   how: `Check if your device can detect your location, and try again.`,
   button: {
     primary: `Try again`,
