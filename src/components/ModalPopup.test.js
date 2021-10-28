f8148dfef5fedec51282d28f35069a777fa5e3a2
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-10-28 20:44:58 +0900

    perf(modalpopup.test): remove redundant imports

--- src/components/ModalPopup.test.js
+++ src/components/ModalPopup.test.js
@@ -1,9 +1,7 @@
 import {render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import {axe} from 'jest-axe';
 
 import {ModalPopup} from './ModalPopup';
-import {buttonLabel} from 'src/utils/uiCopies';
 
 const mockTitle = 'Mock Title';
 const mockProps = {
