d803e4fbfdcead781f6522d98daf06366151b435
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-11-15 18:22:59 +0900

    fix(geolocation): set marker/accuracyCircle to be null after removing them from the map
    prevent the current locaiton marker from reapparing when user zooms out the map after stopping the
    user location tracking

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -56,9 +56,11 @@ export const Controls = ({mapObject}) => {
   function removeUserLocation() {
     if (marker.current) {
       marker.current.setMap(null); // remove the previous current location marker from the map
+      marker.current = null;
     }
     if (accuracyCircle.current) {
       accuracyCircle.current.setMap(null); // remove the previous circle from the map
+      accuracyCircle.current = null;
     }
   }
   function markUserLocation({
