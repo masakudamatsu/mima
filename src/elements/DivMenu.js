9c94dafde87dee05b3f5c8277dbcdc2d30be1b4e
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-11-23 10:53:32 +0900

    fix(menu): remove redundant padding from menu window
    It is no longer necessary as DivPopup now has a padding after commit
    8cfec2f3b4dd8443443ccf2109e5062ac40c605f

--- src/elements/DivMenu.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import {bodyText} from 'src/utils/designtokens';
-import {remify} from 'src/utils/remify';
-
-export const DivMenu = styled.div`
-  padding: ${remify(2 * bodyText.wordSpacing)};
-`;
