138e6249283284ed24f20e6bb23f8a36899a2516
Masa Kudamatsu <masakudamatsu@gmail.com>
2022-08-30 08:14:12 +0900

    refactor(search, searchedplace, placeinfo): turn CSS code for popup background into a utility
    DRY the CSS code

--- src/elements/FormSearch.js
+++ src/elements/FormSearch.js
@@ -1,4 +1,5 @@
 import styled, {css} from 'styled-components';
+import {stylePopupBackground} from 'src/utils/cssUtilities';
 import {
   animation,
   buttonCircle,
@@ -14,41 +15,7 @@ const placeOverMap = `
   z-index: ${zIndex.formSearch};
 `;
 
-const setBackground = `
-  --blur-radius: 8px;
-
-  /* legacy browsers */
-  background-color: var(--popup-background-color-fallback);
-
-  /* Modern browsers */
-  @supports (backdrop-filter: blur(var(--blur-radius))) {
-    background-color: var(--popup-background-color);
-    backdrop-filter: blur(var(--blur-radius));  
-  } 
-
-  /* Firefox and Kai OS */
-  @supports (background-image: -moz-element(#map)) and (not (backdrop-filter: blur(var(--blur-radius)))) {
-    background-color: transparent;
-    /* Blurring the map beneath */
-    &::before {
-      background-attachment: fixed;
-      background-image: -moz-element(#map);
-      content: "";
-      filter: blur(var(--blur-radius));
-      position: absolute;
-      left: 0; right: 0; top: 0; bottom: 0;
-      z-index: -2;
-    }
-    /* Applying translucent white on top */
-    &::after {
-      background-color: var(--popup-background-color);
-      content: "";
-      position: absolute;
-      left: 0; right: 0; top: 0; bottom: 0;
-      z-index: -1;
-    }
-  }
-`;
+const setBackground = stylePopupBackground();
 
 const setSize = `
   height: 100%;
