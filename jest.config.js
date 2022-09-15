4594d6a478dc56b76dfc67ef7977db28fef754ca
Masa Kudamatsu <masakudamatsu@gmail.com>
2022-09-15 16:53:08 +0900

    build(jest): remove redundant configurations
    We always use 'src/...' to import components and utilities.

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,7 @@
-const path = require('path');
-
 module.exports = {
   clearMocks: true, // run jest.clearAllMocks() after each test
   coverageDirectory: 'coverage-jest', // to avoid clashing with Cypress coverage report (https://github.com/bahmutov/cypress-and-jest#jest-init)
-  moduleDirectories: [
-    'node_modules',
-    __dirname,
-    path.join(__dirname, '../src'), // allow imports with absolute paths
-  ],
+  moduleDirectories: ['node_modules', __dirname],
   setupFilesAfterEnv: [
     '@testing-library/jest-dom/extend-expect',
     'jest-axe/extend-expect',
