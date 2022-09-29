4e3068e79cdf585805348b32ec4cf111b8ea644c
Masa Kudamatsu <masakudamatsu@gmail.com>
2022-09-29 17:21:00 +0900

    refactor(places.api): use `switch` keyword to handle request methods
    prepare for handling invalid request methods

--- src/pages/api/places.api.js
+++ src/pages/api/places.api.js
@@ -1,36 +1,44 @@
 const prisma = require('src/utils/prisma');
 
 export default async function handlePlaces(req, res) {
-  if (req.method === 'POST') {
-    const {geometry, properties, type} = req.body;
-    const post = await prisma.place.create({
-      data: {
-        geometry,
-        properties,
-        type,
-      },
-    });
-    res.status(201).json(post);
-  }
-  if (req.method === 'PUT') {
-    const {id, properties} = req.body;
-    const result = await prisma.place.update({
-      where: {
-        id,
-      },
-      data: {
-        properties,
-      },
-    });
-    res.status(200).json(result);
-  }
-  if (req.method === 'DELETE') {
-    const {id} = req.body;
-    await prisma.place.delete({
-      where: {
-        id,
-      },
-    });
-    res.json({success: true});
+  switch (req.method) {
+    case 'POST': {
+      const {geometry, properties, type} = req.body;
+      const post = await prisma.place.create({
+        data: {
+          geometry,
+          properties,
+          type,
+        },
+      });
+      res.status(201).json(post);
+      break;
+    }
+    case 'PUT': {
+      const {id, properties} = req.body;
+      const result = await prisma.place.update({
+        where: {
+          id,
+        },
+        data: {
+          properties,
+        },
+      });
+      res.status(200).json(result);
+      break;
+    }
+    case 'DELETE': {
+      const {id} = req.body;
+      await prisma.place.delete({
+        where: {
+          id,
+        },
+      });
+      res.json({success: true});
+      break;
+    }
+    default: {
+      break;
+    }
   }
 }
