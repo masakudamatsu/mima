ae0bacd57da4829a0f007f443bacff422e16c35b
Masa Kudamatsu <masakudamatsu@gmail.com>
2022-11-01 19:11:19 +0900

    fix(auth0): stop redirecting to subscribe page during the Auth0 authentication flow
    It's no longer necessary as we have removed Auth0 Actions in the login flow which denied access to
    the app if trial period expires

--- src/pages/api/auth/[...auth0].api.js
+++ src/pages/api/auth/[...auth0].api.js
@@ -1,17 +1,3 @@
-import {handleAuth, handleCallback} from '@auth0/nextjs-auth0';
+import {handleAuth} from '@auth0/nextjs-auth0';
 
-export default handleAuth({
-  async callback(req, res) {
-    if (
-      req.query.error === 'access_denied' &&
-      req.query.error_description === 'trial_expiration'
-    ) {
-      res.redirect(307, '/subscribe');
-    }
-    try {
-      await handleCallback(req, res);
-    } catch (error) {
-      res.status(error.status || 500).end(error.message);
-    }
-  },
-});
+export default handleAuth();
