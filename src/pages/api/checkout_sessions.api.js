be495ed65bef3b07c3b3c81f185dfe94f5a4156b
Masa Kudamatsu <masakudamatsu@gmail.com>
2022-11-02 07:25:18 +0900

    fix(stripe): prefill the user's email address in Stripe payment form
    prevent the user from using a different email address for payment
    
    re #311

--- src/pages/api/checkout_sessions.api.js
+++ src/pages/api/checkout_sessions.api.js
@@ -1,6 +1,6 @@
 // adapted from https://stripe.com/docs/checkout/quickstart?client=next
 
-import {withApiAuthRequired} from '@auth0/nextjs-auth0';
+import {withApiAuthRequired, getSession} from '@auth0/nextjs-auth0';
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -13,10 +13,14 @@ export default withApiAuthRequired(async function handleCheckoutSessions(
     res.status(405).end('Method Not Allowed');
     return;
   }
+  // retrieve Auth0 user data from cookie
+  const {user} = getSession(req, res);
   try {
     // Create Checkout Sessions from body params.
     // API reference: https://stripe.com/docs/api/checkout/sessions/create
     const session = await stripe.checkout.sessions.create({
+      // prefill user email address; see https://stripe.com/docs/payments/accept-a-payment?platform=web&ui=checkout#prefill-customer-data
+      customer_email: user.email,
       // define a product
       line_items: [
         {
