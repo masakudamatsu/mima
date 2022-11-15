f842ae92ce364d5b41262eb5d38ccce62d8abd0e
Masa Kudamatsu <masakudamatsu@gmail.com>
2022-11-16 08:03:00 +0900

    test(cypress): make it possible to run all Cypress tests in one go on Cypress desktop app
    The `landmarks.cy.js` is also revised because its "beforeEach" command was outside the describe
    block so it was run for all the tests in other test files.

---
+++ cypress/e2e/all.cy.js
@@ -0,0 +1,16 @@
+// Run all tests with Cypress Desktop app
+// Reference: https://glebbahmutov.com/blog/run-all-specs-cypress-v10/
+
+import './auth.cy';
+import './background.cy';
+import './geolocation.cy';
+import './landmarks.cy';
+import './menu.cy';
+import './metadata.cy';
+import './saved-places.cy';
+import './saving-a-place.cy';
+import './search.cy';
+import './snapshot-geolocation.cy';
+import './snapshot-initial-ui.cy';
+import './snapshot-menu.cy';
+import './snapshot-saved-places.cy';
--- cypress/e2e/landmarks.cy.js
+++ cypress/e2e/landmarks.cy.js
@@ -1,23 +1,25 @@
 import {buttonLabel} from '../../src/utils/uiCopies';
 
-beforeEach(() => {
-  cy.auth();
-});
-it('Landmark roles are given appropriately', () => {
-  cy.visit('/');
-  cy.waitForMapToLoad();
-  cy.log('Main role for map');
-  cy.findByRole('main').contains(/map data/i);
-  cy.log('Navigation role for menu button');
-  cy.findByRole('navigation').findByRole('button', {name: buttonLabel.menu});
-  cy.log('Search role for search button');
-  cy.findByRole('search').findByRole('button', {name: buttonLabel.search});
-  cy.log('Region role for save button');
-  cy.findByRole('region', {name: 'controls'}).findByRole('button', {
-    name: buttonLabel.save,
+describe('Landmark roles', () => {
+  beforeEach(() => {
+    cy.auth();
   });
-  cy.log('Region role for locator button');
-  cy.findByRole('region', {name: 'controls'}).findByRole('button', {
-    name: buttonLabel.locator.default,
+  it('Are given to all the top-level elements', () => {
+    cy.visit('/');
+    cy.waitForMapToLoad();
+    cy.log('Main role for map');
+    cy.findByRole('main').contains(/map data/i);
+    cy.log('Navigation role for menu button');
+    cy.findByRole('navigation').findByRole('button', {name: buttonLabel.menu});
+    cy.log('Search role for search button');
+    cy.findByRole('search').findByRole('button', {name: buttonLabel.search});
+    cy.log('Region role for save button');
+    cy.findByRole('region', {name: 'controls'}).findByRole('button', {
+      name: buttonLabel.save,
+    });
+    cy.log('Region role for locator button');
+    cy.findByRole('region', {name: 'controls'}).findByRole('button', {
+      name: buttonLabel.locator.default,
+    });
   });
 });
