8b74009835887d339af1c43aef82dca5e27d947f
Masa Kudamatsu <masakudamatsu@gmail.com>
2022-11-16 18:37:51 +0900

    fix(auth0): redirect users to the Login page after they log out
    Allow users to learn where to visit next time they want to log in. By default, Auth0 redirects the
    logged-out user to its own login form, which has a temporary URL query string (so the bookmark
    doesn't work).
    
    close #331

--- src/pages/api/auth/[...auth0].api.js
+++ src/pages/api/auth/[...auth0].api.js
@@ -1,3 +1,15 @@
-import {handleAuth} from '@auth0/nextjs-auth0';
+import {handleAuth, handleLogout} from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default handleAuth({
+  async logout(req, res) {
+    try {
+      // source: https://github.com/auth0/nextjs-auth0/issues/583#issuecomment-1020703725
+      await handleLogout(req, res, {
+        returnTo: '/login',
+      });
+    } catch (error) {
+      // source: https://github.com/auth0/nextjs-auth0/blob/main/EXAMPLES.md#customize-handlers-behavior
+      res.status(error.status || 400).end(error.message);
+    }
+  },
+});
