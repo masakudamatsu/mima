c92f1cfafed29799a36de5dee1e53560a81f824f
Masa Kudamatsu <masakudamatsu@gmail.com>
2022-11-17 10:57:52 +0900

    docs(auth0): record what Auth0 user metadata is set for each test user
    refer to it when we need to create an Auth0 tenant for development from scratch again

--- cypress/e2e/all.cy.js
+++ cypress/e2e/all.cy.js
@@ -14,3 +14,48 @@ import './snapshot-geolocation.cy';
 import './snapshot-initial-ui.cy';
 import './snapshot-menu.cy';
 import './snapshot-saved-places.cy';
+
+// Before running these tests, create test users on Auth0 Dashboard
+// (for password, see cypress.env.json)
+//
+// subscribed_user1@gmail.com
+// {
+//   "expiration_date": "2037-12-17T01:24:04.711Z",
+//   "status": "subscribed"
+// }
+//
+// subscribed_user2@yahoo.co.jp
+// {
+//   "expiration_date": "2037-12-17T01:31:14.027Z",
+//   "status": "subscribed"
+// }
+//
+// trial_user@gmail.com
+// {
+//   "expiration_date": "2037-12-08T00:00:58.185Z",
+//   "status": "trial"
+// }
+
+// trial_expired_user@gmail.com
+// {
+//   "expiration_date": "2021-12-17T01:32:29.390Z",
+//   "status": "trial"
+// }
+
+// subscription_expired_user@gmail.com
+// {
+//   "expiration_date": "2022-11-09T23:01:56.180Z",
+//   "status": "subscribed"
+// }
+
+// unpaid_user@gmail.com
+// {
+//   "expiration_date": "2022-11-10T07:13:30.108Z",
+//   "status": "unpaid"
+// }
+
+// cancelled_user@gmail.com
+// {
+//   "expiration_date": "2022-11-10T02:14:37.501Z",
+//   "status": "cancelled"
+// }
