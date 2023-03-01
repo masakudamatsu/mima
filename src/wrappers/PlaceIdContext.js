29fef387ecdeb006d6dbfcbf3534199d52115243
Masa Kudamatsu <masakudamatsu@gmail.com>
2023-03-02 08:32:22 +0900

    docs(search): add as a comment the reference on why we use useMemo in context
    save time to search in Notion notes or on the web

--- src/wrappers/PlaceIdContext.js
+++ src/wrappers/PlaceIdContext.js
@@ -4,6 +4,6 @@ export const PlaceIdContext = createContext();
 
 export function PlaceIdProvider({initialPlaceId = '', ...props}) {
   const [placeId, setPlaceId] = useState(initialPlaceId);
-  const value = useMemo(() => [placeId, setPlaceId], [placeId]);
+  const value = useMemo(() => [placeId, setPlaceId], [placeId]); // see https://github.com/kentcdodds/react-performance/blob/main/src/exercise/05.md
   return <PlaceIdContext.Provider value={value} {...props} />;
 }
