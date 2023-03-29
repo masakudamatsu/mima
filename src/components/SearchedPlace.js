4a27a13d76b250c37f399357895f8334f83102ac
Masa Kudamatsu <masakudamatsu@gmail.com>
2023-03-30 08:21:05 +0900

    refactor(search): deal the happy path first and the sad path next for Place Detail API response
    improve code readability

--- src/components/SearchedPlace.js
+++ src/components/SearchedPlace.js
@@ -71,81 +71,82 @@ export const SearchedPlace = ({mapObject}) => {
     };
     service.getDetails(request, handleResponse);
     function handleResponse(place, status) {
-      if (status !== 'OK' || !place) {
+      if (status === 'OK') {
+        // Retrieve data to be used
+        const searchedPlace = {
+          address: place.formatted_address,
+          // TODO #197 businessStatus: place.business_status,
+          coordinates: {
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+          },
+          name: place.name,
+          url: place.url,
+        };
+
+        // style marker
+        const plusSignIcon = {
+          // source: Material Icons Add: https://fonts.google.com/icons?selected=Material%20Icons%20Outlined%3Aadd%3A
+          height: 24,
+          path: `
+          M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z
+        `,
+          width: 24,
+        };
+        const shapedAsPlusSign = {
+          path: plusSignIcon.path,
+        };
+        const pinnedAtCenter = {
+          anchor: new google.maps.Point(
+            plusSignIcon.width / 2,
+            plusSignIcon.height / 2,
+          ), // to pin the icon at its center, rather than at its top-left (default)
+        };
+        const peach = nightMode
+          ? {
+              fillColor: '#e31081', // hue 328; chroma 82.75; luminance 4.66
+              strokeColor: '#fed0e8', // hue 329; chroma 18.04; luminance 15.4
+            }
+          : {
+              fillColor: '#cf5673', // hue 346, chroma 47.45, luminance 5.23
+              strokeColor: '#82001c', // hue 347, chroma 50.98, luminance 1.97
+            };
+        const colored = {
+          fillOpacity: 1, // to disable the default value of 0
+          ...peach,
+        };
+        const scaled = {
+          scale: 2,
+        };
+        marker.current = new google.maps.Marker({
+          icon: {
+            ...shapedAsPlusSign,
+            ...pinnedAtCenter,
+            ...colored,
+            ...scaled,
+          },
+          optimized: false,
+          position: searchedPlace.coordinates,
+          title: searchedPlace.name,
+        });
+        // eslint-disable-next-line no-loop-func
+        marker.current.addListener('click', () => {
+          mapObject.panTo(searchedPlace.coordinates);
+          mapObject.panBy(0, viewportSize.current.height / 4);
+          setState({status: 'open'});
+        });
+
+        // render marker
+        marker.current.setMap(mapObject);
+        // snap the map to the marker
+        mapObject.panTo(searchedPlace.coordinates);
+        mapObject.panBy(0, viewportSize.current.height / 4);
+        setState({status: 'open', placeData: searchedPlace});
+      } else {
         // TODO #199: Handle error more properly
         console.error('Google Maps Place Details API call has failed.');
         setState({status: 'error', error: status});
       }
-      // Retrieve data to be used
-      const searchedPlace = {
-        address: place.formatted_address,
-        // TODO #197 businessStatus: place.business_status,
-        coordinates: {
-          lat: place.geometry.location.lat(),
-          lng: place.geometry.location.lng(),
-        },
-        name: place.name,
-        url: place.url,
-      };
-
-      // style marker
-      const plusSignIcon = {
-        // source: Material Icons Add: https://fonts.google.com/icons?selected=Material%20Icons%20Outlined%3Aadd%3A
-        height: 24,
-        path: `
-          M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z
-        `,
-        width: 24,
-      };
-      const shapedAsPlusSign = {
-        path: plusSignIcon.path,
-      };
-      const pinnedAtCenter = {
-        anchor: new google.maps.Point(
-          plusSignIcon.width / 2,
-          plusSignIcon.height / 2,
-        ), // to pin the icon at its center, rather than at its top-left (default)
-      };
-      const peach = nightMode
-        ? {
-            fillColor: '#e31081', // hue 328; chroma 82.75; luminance 4.66
-            strokeColor: '#fed0e8', // hue 329; chroma 18.04; luminance 15.4
-          }
-        : {
-            fillColor: '#cf5673', // hue 346, chroma 47.45, luminance 5.23
-            strokeColor: '#82001c', // hue 347, chroma 50.98, luminance 1.97
-          };
-      const colored = {
-        fillOpacity: 1, // to disable the default value of 0
-        ...peach,
-      };
-      const scaled = {
-        scale: 2,
-      };
-      marker.current = new google.maps.Marker({
-        icon: {
-          ...shapedAsPlusSign,
-          ...pinnedAtCenter,
-          ...colored,
-          ...scaled,
-        },
-        optimized: false,
-        position: searchedPlace.coordinates,
-        title: searchedPlace.name,
-      });
-      // eslint-disable-next-line no-loop-func
-      marker.current.addListener('click', () => {
-        mapObject.panTo(searchedPlace.coordinates);
-        mapObject.panBy(0, viewportSize.current.height / 4);
-        setState({status: 'open'});
-      });
-
-      // render marker
-      marker.current.setMap(mapObject);
-      // snap the map to the marker
-      mapObject.panTo(searchedPlace.coordinates);
-      mapObject.panBy(0, viewportSize.current.height / 4);
-      setState({status: 'open', placeData: searchedPlace});
     }
   }, [mapObject, nightMode, placeId, setState]);
 
