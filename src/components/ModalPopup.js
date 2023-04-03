acd7028b120d30f7d5ca0861ff3c06174e2c4486
Masa Kudamatsu <masakudamatsu@gmail.com>
2023-04-04 08:15:11 +0900

    fix(geolocation): trap focus for error messages on geolocation API
    This bug was introduced in the commit before the last

--- src/components/ModalPopup.js
+++ src/components/ModalPopup.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import FocusLock from 'react-focus-lock';
 
 import {DivDialog} from 'src/elements/DivDialog';
 import {DivScrim} from 'src/elements/DivScrim';
@@ -6,20 +7,22 @@ import {DivPopup} from 'src/elements/DivPopup';
 
 export const ModalPopup = ({alert, children, hidden, slideFrom, titleId}) => {
   return (
-    <DivDialog
-      role={alert ? 'alertdialog' : 'dialog'}
-      aria-labelledby={titleId}
-      aria-hidden={hidden}
-    >
-      <DivScrim />
-      <DivPopup
-        data-hidden={hidden}
-        data-slide-from={slideFrom}
-        role="document"
+    <FocusLock disabled={hidden} returnFocus>
+      <DivDialog
+        role={alert ? 'alertdialog' : 'dialog'}
+        aria-labelledby={titleId}
+        aria-hidden={hidden}
       >
-        {children}
-      </DivPopup>
-    </DivDialog>
+        <DivScrim />
+        <DivPopup
+          data-hidden={hidden}
+          data-slide-from={slideFrom}
+          role="document"
+        >
+          {children}
+        </DivPopup>
+      </DivDialog>
+    </FocusLock>
   );
 };
 
