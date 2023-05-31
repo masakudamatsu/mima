77910d5877393b1207a85b6f72a518bb8b13a225
Masa Kudamatsu <masakudamatsu@gmail.com>
2023-05-31 15:15:35 +0900

    fix(signup): render the logo client-side
    prevent it from being rendered in the light/dark mode based on the time of deployment, rather than
    on the time of the user's

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,4 +1,4 @@
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 
 import {NightModeContext} from 'src/wrappers/NightModeContext';
 
@@ -8,6 +8,10 @@ import {VisuallyHidden} from 'src/elements/VisuallyHidden';
 // import PropTypes from 'prop-types';
 
 export const Logo = () => {
+  const [clientSideRendering, setClientSideRendering] = useState(false);
+  useEffect(() => {
+    setClientSideRendering(true);
+  }, []);
   const nightMode = useContext(NightModeContext);
   const logoFilePath = nightMode
     ? '/logo-full-night.svg'
@@ -15,7 +19,7 @@ export const Logo = () => {
   return (
     <>
       <VisuallyHidden as="h1">My Ideal Map</VisuallyHidden>
-      <ImgLogo src={logoFilePath} alt="" />
+      {clientSideRendering && <ImgLogo src={logoFilePath} alt="" />}
     </>
   );
 };
