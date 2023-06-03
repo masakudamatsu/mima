b172699bcba1611e4f91c728aea8aa3adcc97a4e
Masa Kudamatsu <masakudamatsu@gmail.com>
2023-06-03 11:11:13 +0900

    fix(seo): correct the title of the index page shown in a browser tab

--- src/utils/metadata.js
+++ src/utils/metadata.js
@@ -1,5 +1,5 @@
 export const index = {
-  title: 'My Ideal Map App',
+  title: 'My Ideal Map',
   description:
     'Progressive web app that allows you to customize Google Maps with placemarks of your own interest that can be linked to web pages of your choice',
 };
