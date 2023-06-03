260fe2b73aeab04d83f42ac7e195bb2728ba29a4
Masa Kudamatsu <masakudamatsu@gmail.com>
2023-06-03 11:40:21 +0900

    fix(favicon): show the favicon in Safari
    Safari does not support SVG favicons. For Chromium browsers to ignore the .ico file, we need
    `sizes="any"` to the <link> tag that refers to the SVG favicon file (source:
    https://dev.to/xcuses/comment/26cak).

--- src/pages/_document.page.js
+++ src/pages/_document.page.js
@@ -40,7 +40,13 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />{' '}
           {/* Favicon */}
-          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="icon" href="/favicon.ico" sizes="48x48" />
+          <link
+            rel="icon"
+            href="/favicon.svg"
+            sizes="any"
+            type="image/svg+xml"
+          />
         </Head>{' '}
         <body>
           <Main />
