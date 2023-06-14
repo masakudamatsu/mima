c24c3a909642d13ed7e0785b48961037b94a56e8
Masa Kudamatsu <masakudamatsu@gmail.com>
2023-06-14 09:37:02 +0900

    fix(auth0): set free trial period to be 14 days in Auth0
    make it consistent with the sign up button label
    
    re #457

--- src/auth0actions/start-trial-period.test.js
+++ src/auth0actions/start-trial-period.test.js
@@ -1,85 +1,47 @@
-import {addOneMonth} from './start-trial-period';
+import cases from 'jest-in-case';
 
-test('Days 1st to 28th', () => {
-  const year = 2023;
-  const month = 0; // January
-  const day = 1;
-  const result = addOneMonth(new Date(year, month, day));
-  expect(result).toEqual(new Date(year, month + 1, day));
-});
+import {add14days} from './start-trial-period';
 
-test('January 29th, 2023', () => {
-  const year = 2023;
-  const month = 0; // January
-  const day = 29;
-  const result = addOneMonth(new Date(year, month, day));
-  expect(result).toEqual(new Date(year, 1, 28)); // Feb 28, 2023
-});
+cases(
+  'returns the correct outputs:',
+  options => {
+    expect(add14days(options.inputs)).toEqual(options.outputs);
+  },
+  {
+    'Within the same month': {
+      inputs: new Date(2023, 6, 14),
+      outputs: new Date(2023, 6, 28),
+    },
+    'Next month': {
+      inputs: new Date(2023, 7, 28), // August 28, 2023
+      outputs: new Date(2023, 8, 11), // Sept 11, 2023
+    },
+    'Next year': {
+      inputs: new Date(2023, 11, 20), // Dec 20 2023
+      outputs: new Date(2024, 0, 3),
+    },
+  },
+);
 
-test('January 30th, 2023', () => {
-  const year = 2023;
-  const month = 0; // January
-  const day = 30;
-  const result = addOneMonth(new Date(year, month, day));
-  expect(result).toEqual(new Date(year, 1, 28)); // Feb 28, 2023
-});
-
-test('January 31st, 2023', () => {
-  const year = 2023;
-  const month = 0; // January
-  const day = 31;
-  const result = addOneMonth(new Date(year, month, day));
-  expect(result).toEqual(new Date(year, 1, 28)); // Feb 28, 2023
-});
-
-test('March 31st, 2023', () => {
-  const year = 2023;
-  const month = 2;
-  const day = 31;
-  const result = addOneMonth(new Date(year, month, day));
-  expect(result).toEqual(new Date(year, 3, 30)); // April 30, 2023
-});
-
-test('May 31st, 2023', () => {
-  const year = 2023;
-  const month = 4;
-  const day = 31;
-  const result = addOneMonth(new Date(year, month, day));
-  expect(result).toEqual(new Date(year, 5, 30)); // June 30, 2023
-});
-
-test('August 31st, 2023', () => {
-  const year = 2023;
-  const month = 7;
-  const day = 31;
-  const result = addOneMonth(new Date(year, month, day));
-  expect(result).toEqual(new Date(year, 8, 30)); // Sep 30, 2023
-});
-
-test('October 31st, 2023', () => {
-  const year = 2023;
-  const month = 9;
-  const day = 31;
-  const result = addOneMonth(new Date(year, month, day));
-  expect(result).toEqual(new Date(year, 10, 30)); // Nov 30, 2023
-});
-
-test('January 29th, 2024 (leap year)', () => {
-  const year = 2024;
-  const month = 0; // January
-  const day = 29;
-  const result = addOneMonth(new Date(year, month, day));
-  expect(result).toEqual(new Date(year, 1, 29)); // Feb 28, 2024
-});
-
-// // Add 12 months to 29 Feb 2016 -> 28 Feb 2017
-// console.log(addMonths(new Date(2016,1,29),12).toString());
-
-// // Subtract 1 month from 1 Jan 2017 -> 1 Dec 2016
-// console.log(addMonths(new Date(2017,0,1),-1).toString());
-
-// // Subtract 2 months from 31 Jan 2017 -> 30 Nov 2016
-// console.log(addMonths(new Date(2017,0,31),-2).toString());
-
-// // Add 2 months to 31 Dec 2016 -> 28 Feb 2017
-// console.log(addMonths(new Date(2016,11,31),2).toString());
+cases(
+  'returns an error when the input is not a Date object',
+  options => {
+    expect(() => {
+      add14days(options.inputs);
+    }).toThrow(options.outputs);
+  },
+  {
+    missing: {
+      inputs: undefined,
+      outputs: 'The input is missing. Provide a Date object.',
+    },
+    number: {
+      inputs: 1686699076,
+      outputs: 'The input must be a Date object, but you provided a number',
+    },
+    string: {
+      inputs: '2023Jan13',
+      outputs: 'The input must be a Date object, but you provided a string',
+    },
+  },
+);
